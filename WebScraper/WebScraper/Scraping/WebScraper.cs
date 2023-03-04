b96606e0dfe35ff39c77c26b31927910d4548589
TylerWanta <fuzzy1170@gmail.com>
2023-03-04 05:57:23 -0600

    Setup

---
+++ WebScraper/WebScraper/Scraping/WebScraper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WebScraper
+{
+    class WebScraper
+    {
+        private string BaseUrl => "https://www.forexfactory.com/calendar?day=";
+
+        public void ScrapeToday()
+        {
+        }
+
+        public void Scrape(DateTime fromeDate)
+        {
+        }
+    }
+}
---
+++ WebScraper/WebScraper/Service/ScrapingCalendar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using Quartz;
+using Quartz.Impl.Calendar;
+using Quartz.Util;
+
+namespace WebScraper
+{
+    class ScrapingCalendar : BaseCalendar
+    {
+        new public string Description => "Calendar for scraping";
+        new public ICalendar CalendarBase => new HolidayCalendar();
+
+        override public DateTimeOffset GetNextIncludedTimeUtc(DateTimeOffset timeUtc)
+        {
+            // Call base calendar implementation first
+            DateTimeOffset baseTime = base.GetNextIncludedTimeUtc(timeUtc);
+            if (timeUtc != DateTimeOffset.MinValue && baseTime > timeUtc)
+            {
+                timeUtc = baseTime;
+            }
+
+            //apply the timezone
+            timeUtc = TimeZoneUtil.ConvertTime(timeUtc, TimeZone);
+
+            // Get timestamp for 00:00:00, with the correct timezone offset
+            DateTimeOffset day = new DateTimeOffset(timeUtc.Date, timeUtc.Offset);
+
+            while (!IsTimeIncluded(day))
+            {
+                day = day.AddDays(1);
+            }
+
+            return day;
+        }
+
+        override public bool IsTimeIncluded(DateTimeOffset timeUtc)
+        {
+            return CalendarBase.IsTimeIncluded(timeUtc) && timeUtc.DayOfWeek != DayOfWeek.Saturday && timeUtc.DayOfWeek != DayOfWeek.Sunday;
+        }
+    }
+}
