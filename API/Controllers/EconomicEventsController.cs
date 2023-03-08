93e266a2f0c4b1f0918a5cbf9cddcc97cf66d9ed
TylerWanta <fuzzy1170@gmail.com>
2023-03-08 02:00:39 -0600

    Add support for symbol and impact filtering

--- API/Controllers/EconomicEventsController.cs
+++ API/Controllers/EconomicEventsController.cs
@@ -12,27 +12,27 @@ namespace API.Controllers
    
         [HttpGet]
         [Route("from/{utcFrom}")]
-        public async Task<ActionResult> GetEventsFrom(DateTime utcFrom)
+        public async Task<ActionResult> GetEventsFrom(DateTime utcFrom, string symbol, byte? impact)
         {
             if (EconomicEventsDBUsageTracker.ExceededReads)
             {
                 return Conflict(EconomicEventsDBUsageTracker.ExceededReadsJson);
             }
 
-            string json = JsonConvert.SerializeObject(await _economicEventsDB.GetEventsFrom(utcFrom));          
+            string json = JsonConvert.SerializeObject(await _economicEventsDB.GetEventsFrom(utcFrom, symbol, impact));          
             return Ok(json);
         }
 
         [HttpGet]
         [Route("between/{utcFrom}/{utcTo}")]
-        public async Task<ActionResult> GetEvents(DateTime utcFrom, DateTime utcTo)
+        public async Task<ActionResult> GetEvents(DateTime utcFrom, DateTime utcTo, string symbol, byte? impact)
         {
             if (EconomicEventsDBUsageTracker.ExceededReads)
             {
                 return Conflict(EconomicEventsDBUsageTracker.ExceededReadsJson);
             }
 
-            string json = JsonConvert.SerializeObject(await _economicEventsDB.GetEventsBetween(utcFrom, utcTo));
+            string json = JsonConvert.SerializeObject(await _economicEventsDB.GetEventsBetween(utcFrom, utcTo, symbol, impact));
             return Ok(json);
         }
     }
